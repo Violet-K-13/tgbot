7c5481b9b735b55aab31891b0ab13227402d5b77
Violet-K-13 <violetta.khlistun@gmail.com>
2026-06-16 21:07:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 class Settings:
-    BOT_TOKEN = 8715321934:AAEKizhyUypM7mZsnM0SC32u3JawSvMuh58
+    BOT_TOKEN = "8715321934:AAEKizhyUypM7mZsnM0SC32u3JawSvMuh58"
     ADMIN_IDS = []
 settings = Settings()
