774848fcc4da1d84bd4eb0a782a3f60bb83db064
Violet-K-13 <violetta.khlistun@gmail.com>
2026-06-17 22:27:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ class Settings:
     BOT_TOKEN = "8715321934:AAEKizhyUypM7mZsnM0SC32u3JawSvMuh58"
     ADMIN_IDS = []
 settings = Settings()
+CHANNEL_NEWS = -EVTravelClubChannel
+CHANNEL_POLL = -EVTravelClubChannel
+CHANNEL_WISH = -EVTravelClubChannel
+CHANNEL_CLUB = -EVTravelClubChannel
